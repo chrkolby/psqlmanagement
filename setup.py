56e5b5fee46b5debe9517e4e7c5c020b699d615c
chr kolby <chrkolby@gmail.com>
2018-03-27 15:36:43 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='chrkolby',
+      version='0.1',
+      description='Tornado Starter Kit',
+      author='chrkolby',
+      author_email='chrkolby@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['tornado', 'requests', 'beautifulsoup4', 'psycopg2', 'momoko'],
+     )
