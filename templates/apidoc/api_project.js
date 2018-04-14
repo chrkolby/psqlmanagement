b61b1113739e5b258259192a386a263b267b7d87
chr kolby <chrkolby@gmail.com>
2018-04-15 00:08:16 +0200

    fix API url

--- templates/apidoc/api_project.js
+++ templates/apidoc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.1.0",
   "description": "chrkolby API",
   "title": "chrkolby API",
-  "url": "http://localhost:8080/API",
+  "url": "http://tornado-tornado.7e14.starter-us-west-2.openshiftapps.com/API",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-04-14T21:35:11.021Z",
+    "time": "2018-04-14T22:04:38.821Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
